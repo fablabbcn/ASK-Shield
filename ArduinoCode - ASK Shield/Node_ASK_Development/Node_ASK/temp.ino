b8a2244a0e50a1147d24aeb526b241332be24b8f
pral2a <guillem@pral2a.om>
2011-12-18 20:51:46 +0100

    Update ArduinoCode - ASK Shield/Node_ASK_Development/Node_ASK/temp.ino

--- ArduinoCode - ASK Shield/Node_ASK_Development/Node_ASK/temp.ino
+++ ArduinoCode - ASK Shield/Node_ASK_Development/Node_ASK/temp.ino
@@ -1,7 +1,8 @@
 /* ASK Shield - Temperature MCP9700 */
 
 int getTemp (int tempPin, int sensorReadings) {
-  return (computeVolts(getAverage(tempPin, sensorreadings, 1))-0.5*100);
+  return (computeVolts(getAverage(tempPin, sensorreadings, 1)-0.5)*100);
 }
+
 
 
